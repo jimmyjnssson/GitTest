c3c77e7e4aaeb9aa746589a94f72bebec23288f7
Jimmy Jönsson <jimmyjnsson@gmail.com>
2018-04-12 11:36:12 +0200

    First update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,8 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-        System.out.println("Helklo");
+        System.out.println("Helklohh");
+        System.out.println("hej");
 
 
         //Hej
