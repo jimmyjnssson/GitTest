59ffc7770a115b343b9f920831ba2eff34f3ad47
Grov12 <robinforsman@hotmail.com>
2018-04-12 11:42:41 +0200

    test4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,16 +5,9 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-        System.out.println("Helklohh");
+        System.out.println("Hello");
         System.out.println("hej");
 
-        System.out.println("sjkd");
-        System.out.println("Revert");
-
-        System.out.println("Hej");
-
-
-        System.out.println("sdkaj");
         //Hej
     }
 }
