2ec5b63b0590ac4d2949e35d15139f02d85acbe4
danielaug <danne.agesson@hotmail.com>
2018-04-17 00:28:56 +0200

    test commit 2 daniel

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,6 +8,7 @@ public class Main {
         System.out.println("Hello");
         System.out.println("hej");
         System.out.println("test commit");
+        System.out.println("test commit 2");
 
 
         System.out.println("ma");
